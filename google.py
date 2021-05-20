9f2b1093b4e0a1960ff1ce69d59a8a82efe3543c
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-05-20 20:31:51 +0530

    Update google.py

--- google.py
+++ google.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 
 search = input("Input the URL or string you want to search for:")
+
 search_string = search.replace(' ', '+')
+
+#create an instance of Chrome with the path of the driver
 browser = webdriver.Chrome('chromedriver')
 
 for i in range(1):
+	#used to load a website
 	matched_elements = browser.get("https://www.google.com/search?q=" +
 									search_string + "&start=" + str(i))
